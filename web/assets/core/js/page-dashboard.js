5f500e7757655ed61a4afc958c5395de6117abd6
erlindayd <erlindayunilia@gmail.com>
2016-11-24 16:35:32 +0700

    change payload date value

--- web/assets/core/js/page-dashboard.js
+++ web/assets/core/js/page-dashboard.js
@@ -44,7 +44,8 @@ var lastParamChart = {};
 var dtType = '';
 var monthDetailDT = '';
 var projectSelected = '';
-var maxdate = new Date(Date.UTC(2016, 5, 30, 23, 59, 59, 0));
+var maxDateData = new Date(app.getUTCDate(app.currentDateData));
+var maxdate = new Date(Date.UTC(moment(maxDateData).get('year'), maxDateData.getMonth(), maxDateData.getDate(), 23, 59, 59, 0));
 
 lgd.getProjectList = function () {
     app.ajaxPost(viewModel.appName + "/dashboard/getprojectlist", {}, function (res) {
