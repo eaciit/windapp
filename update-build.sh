5f305aba98be0704e2ac2d394b41af7809b58fab
faisal reza <frezadev@faisals-MacBook-Pro.local>
2016-11-29 18:08:35 +0700

    change update build script

--- update-build.sh
+++ update-build.sh
@@ -1,10 +1,10 @@
 #### for wfdemo-git-dev uncomment below part
 
+git checkout .
+
 sudo pkill wfdemo-dev
 rm wfdemo-dev
 rm nohup.out
-
-$GOPATH/bin/gorep -path="." -from="eaciit/wfdemo-git" -to="eaciit/wfdemo-git"
 
 git pull
 
@@ -13,22 +13,3 @@ $GOPATH/bin/gorep -path="." -from="eaciit/wfdemo-git" -to="eaciit/wfdemo-git"
 
 go build -o wfdemo-dev
 nohup ./wfdemo-dev &
-
-
-
-
-# #### for wfdemo-git-oem uncomment below part
-
-# sudo pkill wfdemo-dev
-# rm wfdemo-dev
-# rm nohup.out
-
-# $GOPATH/bin/gorep -path="." -from="eaciit/wfdemo-git-oem" -to="eaciit/wfdemo-git"
-
-# git pull
-
-# $GOPATH/bin/gorep -path="." -from="eaciit/wfdemo-git" -to="eaciit/wfdemo-git-oem"
-
-
-# go build -o wfdemo-dev
-# nohup ./wfdemo-dev &
