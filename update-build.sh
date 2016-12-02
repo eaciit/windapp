b9a683bdf9ad967eb62e998f4c4a616c9f6146ac
faisal reza <frezadev@faisals-MacBook-Pro.local>
2016-12-02 16:46:33 +0700

    fixing update-build

--- update-build.sh
+++ update-build.sh
@@ -2,11 +2,11 @@
 
 git checkout .
 
-sudo pkill wfdemo-git-dev
-rm wfdemo-git-dev
+sudo pkill wfdemo-git-prod
+rm wfdemo-git-prod
 rm nohup.out
 
 $GOPATH/bin/gorep -path="." -from="eaciit/wfdemo-git" -to="eaciit/wfdemo-git-prod"
 
-go build -o wfdemo-git-dev
-nohup ./wfdemo-git-dev
+go build -o wfdemo-git-prod
+nohup ./wfdemo-git-prod
