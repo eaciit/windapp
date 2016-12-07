79a0446562db1b5a83eb84af84fdfc6d9d8d10f1
Rino Sukmandityo <rino.sukmandityo@gmail.com>
2016-12-07 18:24:16 +0700

    no message

--- web/controller/page.go
+++ web/controller/page.go
@@ -172,6 +172,15 @@ func (w *PageController) AnalyticLoss(r *knot.WebContext) interface{} {
 
 	return w.GetParams(r, true)
 }
+
+func (w *PageController) AnalyticDataConsistency(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputTemplate
+	r.Config.LayoutTemplate = LayoutFile
+	r.Config.ViewName = "page-data-consistency.html"
+
+	return w.GetParams(r, true)
+}
+
 func (w *PageController) AnalyticComparison(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputTemplate
 	r.Config.LayoutTemplate = LayoutFile
