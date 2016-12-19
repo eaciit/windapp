e8aca4bc5030b041ede06a48fb9996e726362988
faisal reza <frezadev@faisals-MacBook-Pro.local>
2016-12-19 17:26:17 +0700

    add new collection EventAlarm

--- library/models/event.go
+++ library/models/event.go
@@ -97,3 +97,52 @@ func (m *EventDown) RecordID() interface{} {
 func (m *EventDown) TableName() string {
 	return "EventDown"
 }
+
+type EventAlarm struct {
+	orm.ModelBase `bson:"-",json:"-"`
+	ID            string ` bson:"_id" , json:"_id" `
+	ProjectName   string
+	Turbine       string
+	TimeStart     time.Time
+	TimeStartInt  int64
+	// TimeStartUTC     time.Time
+	DateInfoStart DateInfo
+	// DateInfoStartUTC DateInfo
+	TimeEnd time.Time
+	// TimeEndUTC       time.Time
+	TimeEndInt  int64
+	DateInfoEnd DateInfo
+	// DateInfoEndUTC   DateInfo
+	AlarmDescription string
+	Duration         float64
+	Detail           []EventAlarmDetail
+	/*DownGrid         bool
+	DownEnvironment  bool
+	DownMachine      bool*/
+}
+
+type EventAlarmDetail struct {
+	TimeStamp    time.Time
+	TimeStampInt int64
+	// TimeStampUTC     time.Time
+	DateInfo DateInfo
+	// DateInfoUTC      DateInfo
+	AlarmId          int
+	AlarmDescription string
+	AlarmToggle      bool
+}
+
+func (m *EventAlarm) New() *EventAlarm {
+	milistr := tk.ToString(m.TimeStart.Nanosecond() / 1000000)
+	timeStampStr := m.TimeStart.Format("060102_150405") + "_" + milistr
+	m.ID = timeStampStr + "#" + m.ProjectName + "#" + m.Turbine + "#" + time.Now().Format("060102150405_000000000") + "_" + tk.ToString(rand.Intn(999999))
+	return m
+}
+
+func (m *EventAlarm) RecordID() interface{} {
+	return m.ID
+}
+
+func (m *EventAlarm) TableName() string {
+	return "EventAlarm"
+}
