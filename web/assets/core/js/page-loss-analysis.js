ab14ec078e3a2aae6a9fb427e8cfaddbb915114c
rinosukmandityo <rino.sukmandityo@gmail.com>
2017-01-09 11:29:26 +0700

    creating tooltip shared template for lost energy by DT Type Chart

--- web/assets/core/js/page-loss-analysis.js
+++ web/assets/core/js/page-loss-analysis.js
@@ -1037,9 +1037,9 @@ pg.DTLEbyType = function (dataSource) {
         },
         tooltip: {
             visible: true,
-            format: "{0:n1}",
             background: "rgb(255,255,255, 0.9)",
             shared: true,
+            sharedTemplate: kendo.template($("#templateDTLE").html()),
             color: "#58666e",
             font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
             border: {
