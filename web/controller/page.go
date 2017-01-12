a874f6cd9ce67c92dc23957b7fb52b4fdb3fc438
CHRISTIAN-PC\EACIIT-Christian <christian.bagus@eaciit.com>
2017-01-12 14:29:04 +0700

    Temporary Power Curve Comparison

--- web/controller/page.go
+++ web/controller/page.go
@@ -144,6 +144,14 @@ func (w *PageController) AnalyticPCMonthly(r *knot.WebContext) interface{} {
 	return w.GetParams(r, true)
 }
 
+func (w *PageController) AnalyticPCComparison(r *knot.WebContext) interface{} {
+	r.Config.OutputType = knot.OutputTemplate
+	r.Config.LayoutTemplate = LayoutFile
+	r.Config.ViewName = "page-analytic-power-curve/comparison.html"
+
+	return w.GetParams(r, true)
+}
+
 /*func (w *PageController) AnalyticDgrScada(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputTemplate
 	r.Config.LayoutTemplate = LayoutFile
