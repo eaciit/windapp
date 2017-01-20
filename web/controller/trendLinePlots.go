5d7a3ed436a4419273402de19bf360f65c5ed81a
CHRISTIAN-PC\EACIIT-Christian <christian.bagus@eaciit.com>
2017-01-20 13:23:39 +0700

    New Page - Analysis Toolbox > Trend Line Plots

---
+++ web/controller/trendLinePlots.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	// . "eaciit/wfdemo-git/library/core"
+	// hp "eaciit/wfdemo-git/library/helper"
+	// . "eaciit/wfdemo-git/library/models"
+	// "eaciit/wfdemo-git/web/helper"
+	// "strings"
+	// "time"
+
+	// "github.com/eaciit/dbox"
+
+	// "github.com/eaciit/knot/knot.v1"
+	// tk "github.com/eaciit/toolkit"
+)
+
+type TrendLinePlotsController struct {
+	App
+}
+
+func CreateTrendLinePlotsController() *TrendLinePlotsController {
+	var controller = new(TrendLinePlotsController)
+	return controller
+}
