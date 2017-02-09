3cc486f31406b49ffe7c6bfa785655f7d53782a8
rinosukmandityo <rino.sukmandityo@gmail.com>
2017-02-09 14:35:29 +0700

    seperate windrose into new file for easy maintenance

--- web/assets/core/js/page-analytic-meteorology.js
+++ web/assets/core/js/page-analytic-meteorology.js
@@ -23,20 +23,12 @@ pm.breakDownList = ko.observableArray([
     { "value": "turbine", "text": "Turbine" },
 ]);
 pm.dataSourceAverage = ko.observableArray();
-pm.dataWindrose = ko.observableArray([]);
-pm.dataWindroseGrid = ko.observableArray([]);
-pm.dataWindroseEachTurbine = ko.observableArray([]);
-pm.sectorDerajat = ko.observable(0);
 pm.sectionsBreakdownList = ko.observableArray([
     { "text": 36, "value": 36 },
     { "text": 24, "value": 24 },
     { "text": 12, "value": 12 },
 ]);
-
-var maxValue = 0;
 var colorFieldsWR = ["#000292", "#005AFD", "#25FEDF", "#EBFE14", "#FF4908", "#9E0000", "#ff0000"];
-var listOfChart = [];
-var listOfButton = {};
 var listOfCategory = [
     { "category": "0 to 4m/s", "color": colorFieldsWR[0] },
     { "category": "4 to 8m/s", "color": colorFieldsWR[1] },
@@ -192,185 +184,13 @@ pm.AverageWindSpeed = function() {
 
 }
 
-// WIND ROSE
-pm.ExportWindRose = function () {
-    var chart = $("#wr-chart").getKendoChart();
-    chart.exportPDF({ paperSize: "auto", margin: { left: "1cm", top: "1cm", right: "1cm", bottom: "1cm" } }).done(function (data) {
-        kendo.saveAs({
-            dataURI: data,
-            fileName: "WindRose.pdf",
-        });
-    });
-}
-pm.initChart = function () {
-    listOfChart = [];
-    var breakDownVal = $("#nosection").data("kendoDropDownList").value();
-    var stepNum = 1
-    var gapNum = 1
-    if (breakDownVal == 36) {
-        stepNum = 3
-        gapNum = 0
-    } else if (breakDownVal == 24) {
-        stepNum = 2
-        gapNum = 0
-    } else if (breakDownVal == 12) {
-        stepNum = 1
-        gapNum = 0
-    }
-
-    $.each(pm.dataWindroseEachTurbine(), function (i, val) {
-        var name = val.Name
-        if (name == "MetTower") {
-            name = "Met Tower"
-        }
-
-        var idChart = "#chart-" + val.Name
-        listOfChart.push(idChart);
-        // var pWidth = $('body').width() * 0.235;//$('body').width() * ($(idChart).closest('div.windrose-item').width() - 2) / 100;
-        var pWidth = 290;
-
-        $(idChart).kendoChart({
-            theme: "nova",
-            chartArea: {
-                width: pWidth,
-                height: pWidth,
-                padding: 25
-            },
-
-            title: {
-                text: name,
-                font: '13px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
-            },
-            legend: {
-                position: "bottom",
-                labels: {
-                    template: "#= (series.data[0] || {}).WsCategoryDesc #"
-                },
-                visible: false,
-            },
-            dataSource: {
-                data: val.Data,
-                group: {
-                    field: "WsCategoryNo",
-                    dir: "asc"
-                },
-                sort: {
-                    field: "DirectionNo",
-                    dir: "asc"
-                }
-            },
-            seriesColors: colorFieldsWR,
-            series: [{
-                type: "radarColumn",
-                stack: true,
-                field: "Contribution",
-                gap: gapNum,
-                border: {
-                    width: 1,
-                    color: "#7f7f7f",
-                    opacity: 0.5
-                },
-            }],
-            categoryAxis: {
-                field: "DirectionDesc",
-                visible: true,
-                majorGridLines: {
-                    visible: true,
-                    step: stepNum
-                },
-                labels: {
-                    font: '11px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
-                    visible: true,
-                    step: stepNum
-                }
-            },
-            valueAxis: {
-                labels: {
-                    template: kendo.template("#= kendo.toString(value, 'n0') #%"),
-                    font: '9px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif'
-                },
-                majorUnit: 10,
-                max: maxValue,
-                min: 0
-            },
-            tooltip: {
-                visible: true,
-                template: "#= category #"+String.fromCharCode(176)+" (#= dataItem.WsCategoryDesc #) #= kendo.toString(value, 'n2') #% for #= kendo.toString(dataItem.Hours, 'n2') # Hours",
-                background: "rgb(255,255,255, 0.9)",
-                color: "#58666e",
-                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
-                border: {
-                    color: "#eee",
-                    width: "2px",
-                },
-            }
-        });
-    });
-}
-
-pm.ChangeSector = function(){
-    pm.isFirstWindRose(true);
-    pm.WindRose();
-}
-
-pm.WindRose = function(){
-    app.loading(true);
-    fa.LoadData();
-    if(pm.isFirstWindRose() === true){
-        setTimeout(function () {
-            var breakDownVal = $("#nosection").data("kendoDropDownList").value();
-            var secDer = 360 / breakDownVal;
-            pm.sectorDerajat(secDer);
-            var param = {
-                period: fa.period,
-                dateStart: fa.dateStart,
-                dateEnd: fa.dateEnd,
-                turbine: fa.turbine,
-                project: fa.project,
-                breakDown: breakDownVal,
-            };
-            toolkit.ajaxPost(viewModel.appName + "analyticwindrose/getflexidataeachturbine", param, function (res) {
-                if (!app.isFine(res)) {
-                    app.loading(false);
-                    return;
-                }
-                if (res.data.WindRose != null) {
-                    var metData = res.data.WindRose;
-                    maxValue = res.data.MaxValue;
-                    pm.dataWindroseEachTurbine(metData);
-                    pm.initChart();
-                }
-
-                app.loading(false);
-                pm.isFirstWindRose(false);
-
-            })
-        }, 300);
-        var metDate = 'Data Available (<strong>MET</strong>) from: <strong>' + availDateList.availabledatestartmet + '</strong> until: <strong>' + availDateList.availabledateendmet + '</strong>'
-        var scadaDate = ' | (<strong>SCADA</strong>) from: <strong>' + availDateList.availabledatestartscada + '</strong> until: <strong>' + availDateList.availabledateendscada + '</strong>'
-        $('#availabledatestart').html(metDate);
-        $('#availabledateend').html(scadaDate);
-    }else{
-        var metDate = 'Data Available (<strong>MET</strong>) from: <strong>' + availDateList.availabledatestartmet + '</strong> until: <strong>' + availDateList.availabledateendmet + '</strong>'
-        var scadaDate = ' | (<strong>SCADA</strong>) from: <strong>' + availDateList.availabledatestartscada + '</strong> until: <strong>' + availDateList.availabledateendscada + '</strong>'
-        $('#availabledatestart').html(metDate);
-        $('#availabledateend').html(scadaDate);
-        setTimeout(function(){
-            $.each(listOfChart, function(idx, elem){
-                $(elem).data("kendoChart").refresh();
-            });
-            app.loading(false);
-        }, 300);
-    }
-}
-
 // WIND ROSE COMPARISON
 pm.isFirstWindRoseComparison = ko.observable(true);
 pm.sectorDerajatComparison = ko.observable(0);
 pm.dataWindroseComparison = ko.observableArray([]);
 var listOfChartComparison = [];
 var listOfButtonComparison = {};
-pm.showHideLegendComparison = function (index) {
+/*pm.showHideLegendComparison = function (index) {
     var idName = "btn" + index;
     listOfButtonComparison[idName] = !listOfButtonComparison[idName];
     if (listOfButtonComparison[idName] == false) {
@@ -384,7 +204,7 @@ pm.showHideLegendComparison = function (index) {
           $(idChart).data("kendoChart").refresh();
         }
     });
-}
+}*/
 
 pm.InitTurbineListCompare = function () {
     if (pm.dataWindroseComparison().Data.length > 1) {
@@ -514,8 +334,8 @@ pm.initChartWRC = function () {
                 font: '11px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif'
             },
             majorUnit: majorUnit,
-            max: nilaiMax,
-            min: 0
+            // max: nilaiMax,
+            // min: 0
         },
         tooltip: {
             visible: true,
@@ -776,22 +596,6 @@ pm.showHideAllLegend = function (e) {
     $("#windDistribution").data("kendoChart").redraw();
 }
 
-pm.showHideLegendWR = function (index) {
-    var idName = "btn" + index;
-    listOfButton[idName] = !listOfButton[idName];
-    if (listOfButton[idName] == false) {
-        $("#" + idName).css({ 'background': '#8f8f8f', 'border-color': '#8f8f8f' });
-    } else {
-        $("#" + idName).css({ 'background': colorFieldsWR[index], 'border-color': colorFieldsWR[index] });
-    }
-    $.each(listOfChart, function (idx, idChart) {
-       if($(idChart).data("kendoChart").options.series.length - 1 >= index) {
-          $(idChart).data("kendoChart").options.series[index].visible = listOfButton[idName];
-          $(idChart).data("kendoChart").refresh();
-        }
-    });
-}
-
 pm.showHideLegend = function (idx) {
     var stat = false;
 
@@ -995,7 +799,7 @@ $(function(){
             var idName = "btn" + idx;
             listOfButton[idName] = true;
             $("#legend-list").append(
-                '<button id="' + idName + '" class="btn btn-default btn-sm btn-legend" type="button" onclick="pm.showHideLegendWR(' + idx + ')" style="border-color:' + val.color + ';background-color:' + val.color + ';"></button>' +
+                '<button id="' + idName + '" class="btn btn-default btn-sm btn-legend" type="button" onclick="wr.showHideLegendWR(' + idx + ')" style="border-color:' + val.color + ';background-color:' + val.color + ';"></button>' +
                 '<span class="span-legend">' + val.category + '</span>'
             );
         });
