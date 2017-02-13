43274209770c358a5c7bfb2e80a2bdfc5e2ae081
rinosukmandityo <rino.sukmandityo@gmail.com>
2017-02-13 09:50:40 +0700

    separate lost energy tab on loss analysis page into new file

--- web/controller/page.go
+++ web/controller/page.go
@@ -225,6 +225,7 @@ func (w *PageController) AnalyticLoss(r *knot.WebContext) interface{} {
 		"page-loss-analysis/static-view.html",
 		"page-loss-analysis/downtime.html",
 		"page-loss-analysis/availability.html",
+		"page-loss-analysis/lost-energy.html",
 	}...)
 	r.Config.ViewName = "page-loss-analysis.html"
 
