095fe7ae21b98f24ffc10d3753854878142161e8
Erlinda Damayanti <erlindayunilia@gmail.com>
2017-02-20 14:02:15 +0700

    New Req - Losses & Efficiency > add chart under static view

--- web/assets/core/js/page-loss-analysis/static-view.js
+++ web/assets/core/js/page-loss-analysis/static-view.js
@@ -4,6 +4,341 @@ viewModel.StaticView = new Object();
 var sv = viewModel.StaticView;
 
 sv.type = ko.observable();
+sv.isGrid = ko.observable(true);
+
+
+sv.refreshView = function(view){
+    app.loading(true);
+    setTimeout(function(){
+        if(view == "gridStatic"){
+            $('#lossGrid').data("kendoGrid").refresh();
+        }else{
+             $("#staticProductionChart").data("kendoChart").refresh();
+             $("#staticDowntimeChart").data("kendoChart").refresh();
+             $("#staticLossChart").data("kendoChart").refresh();
+        }
+        app.loading(false);
+    },200);
+}
+
+sv.getChartView = function(gData){
+    $("#staticProductionChart").replaceWith('<div id="staticProductionChart"></div>');
+    $("#staticProductionChart").kendoChart({
+        theme:"flat",
+        dataSource: {
+            data: gData,
+            sort:{
+                field: "Id",
+                dir: "asc"
+            }
+        },
+        legend: {
+            position: "top",
+            visible: true,
+            labels: {
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            }
+        },
+        chartArea :{
+            height: 200, 
+            margin : 0,
+            padding: 0,
+            background: "transparent",
+        },
+        series: [{
+            field: "Production",
+            name: "Production",
+        }],
+        seriesColors: colorField,
+        valueAxes: [{
+            visible: true,
+            line: {
+                visible: false
+            },
+            labels: {
+                format: "{0}",
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            },
+            axisCrossingValue: -10,
+            majorGridLines: {
+                visible: true,
+                color: "#eee",
+                width: 0.8,
+            },
+            title: { text: "Production (MWh)", font: '14px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif' },
+        }],
+        categoryAxis: {
+            field: "Id",
+            labels: {
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            },
+            justified: true,
+            majorGridLines: {
+                visible: false
+            },
+        },
+        tooltip: {
+            visible: true,
+            template: "#= series.name # at #= category # : #= kendo.toString(value, 'n2')# MWh",
+            shared: false,
+            background: "rgb(255,255,255, 0.9)",
+            color: "#58666e",
+            font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            border: {
+                color: "#eee",
+                width: "2px",
+            },
+
+        },
+    });
+
+    $("#staticDowntimeChart").replaceWith('<div id="staticDowntimeChart"></div>');
+    $("#staticDowntimeChart").kendoChart({
+        theme:"flat",
+        dataSource: {
+            data: gData,
+            sort:{
+                field: "Id",
+                dir: "asc"
+            }
+        },
+        legend: {
+            position: "top",
+            visible: true,
+            labels: {
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            }
+        },
+        chartArea :{
+            height: 200, 
+            margin : 0,
+            padding: 0,
+            background: "transparent",
+        },
+        seriesDefaults:{
+            stack: true,
+        },
+        series: [{
+            field: "MachineDownHours",
+            name: "Machine",
+        },{
+            field: "GridDownHours",
+            name: "Grid",
+        }],
+        seriesColors: colorField,
+        valueAxes: [{
+            visible: true,
+            line: {
+                visible: false
+            },
+            labels: {
+                format: "{0}",
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            },
+            axisCrossingValue: -10,
+            majorGridLines: {
+                visible: true,
+                color: "#eee",
+                width: 0.8,
+            },
+            title: { text: "Downtime Duration (Hrs)", font: '14px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif' },
+        }],
+        categoryAxis: {
+            field: "Id",
+            labels: {
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            },
+            justified: true,
+            majorGridLines: {
+                visible: false
+            },
+        },
+        tooltip: {
+            visible: true,
+            template: "#= series.name # at #= category # : #= kendo.toString(value, 'n2')# MWh",
+            shared: false,
+            background: "rgb(255,255,255, 0.9)",
+            color: "#58666e",
+            font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            border: {
+                color: "#eee",
+                width: "2px",
+            },
+
+        },
+    });
+
+    $("#staticLossChart").replaceWith('<div id="staticLossChart"></div>');
+    $("#staticLossChart").kendoChart({
+        theme:"flat",
+        dataSource: {
+            data: gData,
+            sort:{
+                field: "Id",
+                dir: "asc"
+            }
+        },
+        legend: {
+            position: "top",
+            visible: true,
+            labels: {
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            }
+        },
+        chartArea :{
+            height: 200, 
+            margin : 0,
+            padding: 0,
+            background: "transparent",
+        },
+        seriesDefaults:{
+            stack: true,
+        },
+        series: [{
+            field: "EnergyyMD",
+            name: "Machine",
+        },{
+            field: "EnergyyGD",
+            name: "Grid",
+        }],
+        seriesColors: colorField,
+        valueAxes: [{
+            visible: true,
+            line: {
+                visible: false
+            },
+            labels: {
+                format: "{0}",
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            },
+            axisCrossingValue: -10,
+            majorGridLines: {
+                visible: true,
+                color: "#eee",
+                width: 0.8,
+            },
+            name: "availline",
+            title: { text: "Energy Loss (MWh)", font: '14px Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif' },
+        }],
+        categoryAxis: {
+            field: "Id",
+            labels: {
+                font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            },
+            justified: true,
+            majorGridLines: {
+                visible: false
+            },
+        },
+        tooltip: {
+            visible: true,
+            template: "#= series.name # at #= category # : #= kendo.toString(value, 'n2')# MWh",
+            shared: false,
+            background: "rgb(255,255,255, 0.9)",
+            color: "#58666e",
+            font: 'Source Sans Pro, Lato , Open Sans , Helvetica Neue, Arial, sans-serif',
+            border: {
+                color: "#eee",
+                width: "2px",
+            },
+
+        },
+    });
+
+    app.loading(false);
+}
+
+sv.getGridView = function(gData){
+    $('#lossGrid').html("");
+    $('#lossGrid').kendoGrid({
+        dataSource: {
+            data: gData,
+            pageSize: 10,
+            aggregate: [
+                { field: "Production", aggregate: "sum" },
+                { field: "LossEnergy", aggregate: "sum" },
+                { field: "MachineDownHours", aggregate: "sum" },
+                { field: "GridDownHours", aggregate: "sum" },
+                { field: "EnergyyMD", aggregate: "sum" },
+                { field: "EnergyyGD", aggregate: "sum" },
+                { field: "ElectricLoss", aggregate: "sum" },
+                { field: "PCDeviation", aggregate: "sum" },
+                { field: "Others", aggregate: "sum" },
+            ]
+        },
+        groupable: false,
+        sortable: true,
+        filterable: false,
+        // height: $(".content-wrapper").height() - ($("#filter-analytic").height()+209),
+        height: 399,
+        pageable: {
+            pageSize: 10,
+            input: true, 
+        },
+        columns: [
+            { title: sv.type,field: "Id",width: 100,attributes: {style: "text-align:center;"},headerAttributes: {style: "text-align:center;"},footerTemplate: "<center>Total (All Turbines)</center>"}, 
+            { title: "Production (MWh)", headerAttributes: { tyle: "text-align:center;"}, field: "Production",width: 100,attributes: { class: "align-center" },format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" }, 
+            { title: "Lost Energy (MWh)",headerAttributes: {style: "text-align:center;"},field: "LossEnergy", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"},
+            {
+                title: "Downtime : Duration (Hrs)",
+                headerAttributes: {
+                    style: 'font-weight: bold; text-align: center;'
+                },
+                columns: [
+                    {
+                        title: "Machine",
+                        headerAttributes: { style: "text-align:center;" },
+                        field: "MachineDownHours", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" 
+                    },
+                    {
+                        title: "Grid",
+                        headerAttributes: { style: "text-align:center;" },
+                        field: "GridDownHours", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
+                    },
+                ]
+            }, {
+                title: "Downtime : Energy Loss (MWh)",
+                headerAttributes: {
+                    style: 'font-weight: bold; text-align: center;'
+                },
+                columns: [
+                    {
+                        title: "Machine",
+                        headerAttributes: { style: "text-align:center;" },
+                        field: "EnergyyMD", width: 100, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" 
+                    },
+                    {
+                        title: "Grid",
+                        headerAttributes: { style: "text-align:center;" },
+                        field: "EnergyyGD", width: 100, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" 
+                    },
+                ]
+            }, {
+                title: "Electrical Losses (MWh)",
+                headerAttributes: {
+                    style: "text-align:center;"
+                },
+                field: "ElectricLoss", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
+            }, {
+                title: "Power Curve Deviation (MW)", //Sepertinya ini MW
+                headerAttributes: {
+                    style: "text-align:center;"
+                },
+                field: "PCDeviation", width: 120, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
+            }, {
+                title: "Others (MWh)", //Sepertinya ini KWh
+                headerAttributes: {
+                    style: "text-align:center;"
+                },
+                field: "Others", width: 100, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
+            }
+        ],
+        dataBound: function(){
+             app.loading(false);
+             pg.isFirstStaticView(false);
+        }
+    });
+}
 
 sv.StaticView = function(){
     fa.LoadData();
@@ -23,101 +358,15 @@ sv.StaticView = function(){
             }
 
             var gData = res.data.Data
+            sv.getChartView(gData);
+            sv.getGridView(gData);
 
-            $('#lossGrid').html("");
-            $('#lossGrid').kendoGrid({
-                dataSource: {
-                    data: gData,
-                    pageSize: 10,
-                    aggregate: [
-                        { field: "Production", aggregate: "sum" },
-                        { field: "LossEnergy", aggregate: "sum" },
-                        { field: "MachineDownHours", aggregate: "sum" },
-                        { field: "GridDownHours", aggregate: "sum" },
-                        { field: "EnergyyMD", aggregate: "sum" },
-                        { field: "EnergyyGD", aggregate: "sum" },
-                        { field: "ElectricLoss", aggregate: "sum" },
-                        { field: "PCDeviation", aggregate: "sum" },
-                        { field: "Others", aggregate: "sum" },
-                    ]
-                },
-                groupable: false,
-                sortable: true,
-                filterable: false,
-                // height: $(".content-wrapper").height() - ($("#filter-analytic").height()+209),
-                height: 399,
-                pageable: {
-                    pageSize: 10,
-                    input: true, 
-                },
-                columns: [
-                    { title: sv.type,field: "Id",width: 100,attributes: {style: "text-align:center;"},headerAttributes: {style: "text-align:center;"},footerTemplate: "<center>Total (All Turbines)</center>"}, 
-                    { title: "Production (MWh)", headerAttributes: { tyle: "text-align:center;"}, field: "Production",width: 100,attributes: { class: "align-center" },format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" }, 
-                    { title: "Lost Energy (MWh)",headerAttributes: {style: "text-align:center;"},field: "LossEnergy", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"},
-                    {
-                        title: "Downtime : Duration (Hrs)",
-                        headerAttributes: {
-                            style: 'font-weight: bold; text-align: center;'
-                        },
-                        columns: [
-                            {
-                                title: "Machine",
-                                headerAttributes: { style: "text-align:center;" },
-                                field: "MachineDownHours", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" 
-                            },
-                            {
-                                title: "Grid",
-                                headerAttributes: { style: "text-align:center;" },
-                                field: "GridDownHours", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate: "<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
-                            },
-                        ]
-                    }, {
-                        title: "Downtime : Energy Loss (MWh)",
-                        headerAttributes: {
-                            style: 'font-weight: bold; text-align: center;'
-                        },
-                        columns: [
-                            {
-                                title: "Machine",
-                                headerAttributes: { style: "text-align:center;" },
-                                field: "EnergyyMD", width: 100, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" 
-                            },
-                            {
-                                title: "Grid",
-                                headerAttributes: { style: "text-align:center;" },
-                                field: "EnergyyGD", width: 100, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>" 
-                            },
-                        ]
-                    }, {
-                        title: "Electrical Losses (MWh)",
-                        headerAttributes: {
-                            style: "text-align:center;"
-                        },
-                        field: "ElectricLoss", width: 100, attributes: { class: "align-center" }, format: "{0:n2}",footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
-                    }, {
-                        title: "Power Curve Deviation (MW)", //Sepertinya ini MW
-                        headerAttributes: {
-                            style: "text-align:center;"
-                        },
-                        field: "PCDeviation", width: 120, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
-                    }, {
-                        title: "Others (MWh)", //Sepertinya ini KWh
-                        headerAttributes: {
-                            style: "text-align:center;"
-                        },
-                        field: "Others", width: 100, attributes: { class: "align-center" }, format: "{0:n2}", footerTemplate:"<div style='text-align:center'>#=kendo.toString(sum, 'n2')#</div>"
-                    }
-                ],
-                dataBound: function(){
-                     app.loading(false);
-                     pg.isFirstStaticView(false);
-                }
-            })
         });
+
         $('#availabledatestart').html(pg.availabledatestartscada());
         $('#availabledateend').html(pg.availabledateendscada());
     }else{
-        $("#lossGrid").data("kendoGrid").refresh();
+        sv.refreshView($('input[name=convertStatic]:checked').val());
         $('#availabledatestart').html(pg.availabledatestartscada());
         $('#availabledateend').html(pg.availabledateendscada());
     }
