832d9fac8af0f521c6ce47f2ed42a3a90cc0960f
Erlinda Damayanti <erlindayunilia@gmail.com>
2017-03-23 11:15:39 +0700

    bug fix

--- web/assets/core/js/page-time-series.js
+++ web/assets/core/js/page-time-series.js
@@ -363,8 +363,12 @@ pg.getDataStockChart = function(param, idBtn){
     var url = "timeseries/getdatahfd";
     if($('input[name="chk-column-live"]:checked').length > 0){
         pg.live(true);
+        pg.rangeData(true);
+        pg.errorValue(true);
     }else{
         pg.live(false);
+        pg.rangeData(true);
+        pg.errorValue(true);
     }
 
 
@@ -431,14 +435,22 @@ pg.createLiveChart = function(IsHour){
         IsHour : IsHour,
     };
 
+    var dateStart, dateEnd; 
     $("#chartTimeSeries").html("");
         toolkit.ajaxPost(viewModel.appName + "timeseries/getdatahfd", param, function (res) {
         if (!app.isFine(res)) {
             return;
         }
 
+
         var data = res.data.Data.Chart;
         var periods = res.data.Data.PeriodList;
+
+        dateStart = new Date(new Date(Math.round(data[0].data[0][0])).toUTCString());
+        dateEnd = new Date(new Date(Math.round(data[0].data[0][0])).toUTCString());
+
+        console.log(dateEnd);
+
         breaks = res.data.Data.Breaks;
 
         pg.generateSeriesOption(data, periods);
@@ -482,8 +494,8 @@ pg.createLiveChart = function(IsHour){
                             var paramX = {
                                 period: fa.period,
                                 Turbine: [fa.turbine],
-                                DateStart: new Date(),
-                                DateEnd: new Date(),
+                                DateStart: dateStart,
+                                DateEnd: dateEnd,
                                 Project: fa.project,
                                 PageType: "LIVE",
                                 DataType: pg.dataType() ,
@@ -499,10 +511,8 @@ pg.createLiveChart = function(IsHour){
                                 var seriesData = chart.series; 
                                 var results = res.data.Data.Chart;
                                 if(results.length > 0){
-                                    $.each(seriesOptions, function(id, val){
-                                        chart.series[id].setData(val.data, true, true, false);
-                                    });
-
+                                    dateStart = new Date(new Date(Math.round(results[0].data[0][0])).toUTCString());
+                                    dateEnd = new Date(new Date(Math.round(results[0].data[0][0])).toUTCString());
                                     $.each(seriesOptions, function(i, res){
                                        $.each(results, function(id, val){
                                             if(res.name == val.name){
@@ -511,7 +521,9 @@ pg.createLiveChart = function(IsHour){
                                             }
                                        })
                                     });
+                                    console.log(dateEnd);
                                 }else{
+                                    console.log(dateEnd);
                                     return false;
                                 }
 
@@ -810,6 +822,8 @@ $(document).ready(function () {
 
     $('#btnRefresh').on('click', function () {
         pg.getDataStockChart("refresh");
+        pg.rangeData(true);
+        pg.errorValue(true);
     });
 
     setTimeout(function () {
