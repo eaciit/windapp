a8201ad7476f6432de236949fc684d699e7886a0
rinosukmandityo <rino.sukmandityo@gmail.com>
2017-08-07 16:24:48 +0700

    change data structure of get machine & grid availability without reduce speed of loading data

--- web/controller/dashboard.go
+++ web/controller/dashboard.go
@@ -2465,33 +2465,51 @@ func getMGAvailability(p *PayloadDashboard) (machineResult []tk.M, gridResult []
 			scada.Unset("minutes")
 		}
 	}
-
+	orderNo := 0
+	ids := tk.M{}
 	for _, res := range result {
-		id := res.Get("_id")
-		machineResult = append(machineResult, tk.M{"_id": id, "result": res.GetFloat64("machineResult")})
-		gridResult = append(gridResult, tk.M{"_id": id, "result": res.GetFloat64("gridResult")})
+		orderNo++
+		ids, _ = tk.ToM(res.Get("_id"))
+		machineResult = append(machineResult, tk.M{
+			"DataId":  tk.ToString(ids.GetInt("id1")),
+			"Title":   ids.GetString("id2"),
+			"OrderNo": orderNo,
+			"Value":   res.GetFloat64("machineResult"),
+			"Project": ids.GetString("id3"),
+		})
+		gridResult = append(gridResult, tk.M{
+			"DataId":  tk.ToString(ids.GetInt("id1")),
+			"Title":   ids.GetString("id2"),
+			"OrderNo": orderNo,
+			"Value":   res.GetFloat64("gridResult"),
+			"Project": ids.GetString("id3"),
+		})
 	}
+
+	mrTmp := []tk.M{}
+	grTmp := []tk.M{}
 
 	if len(machineResult) > (len(projects) * 12) {
 		length := len(machineResult)
 		div := (length / len(projects))
 
-		mrTmp := []tk.M{}
-		grTmp := []tk.M{}
-
 		for i := 1; i < len(projects)+1; i++ {
 			offerX := (div * i) - 12
 			// log.Printf("> %v | %v | %v | %v \n", div, div*i, offerX, offerX+12)
 
-			mrTmp = append(mrTmp, machineResult[offerX:offerX+12]...)
-			grTmp = append(grTmp, gridResult[offerX:offerX+12]...)
+			mrTmp = append(mrTmp, tk.M{
+				"Project": machineResult[offerX].GetString("Project"),
+				"Details": machineResult[offerX : offerX+12],
+			})
+			grTmp = append(grTmp, tk.M{
+				"Project": gridResult[offerX].GetString("Project"),
+				"Details": gridResult[offerX : offerX+12],
+			})
 		}
-
 		machineResult = mrTmp
 		gridResult = grTmp
 	}
 
-	// tk.Println()
 	return
 }
 
