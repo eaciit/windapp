7cf01d5fee6817b2966758630689abf2db1f7e9d
alipsidikp <alip.sidik.p@gmail.com>
2017-09-26 13:33:27 +0700

    model monitoring notification log

---
+++ library/models/monitoringnotif.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/eaciit/orm"
+)
+
+type MonitoringNotification struct {
+	orm.ModelBase `bson:"-",json:"-"`
+	Id            string
+	ProjectName   string
+	Turbine       string
+	GTags         string
+	Tags          string
+	TimeStart     time.Time
+	TimeEnd       time.Time
+	Status        bool
+	IsError       bool
+	CompareVal    float64
+	Value         float64
+	LastValue     float64
+	NoteStart     string
+	NoteEnd       string
+}
+
+func (m *MonitoringNotification) New() *MonitoringNotification {
+	m.Id = fmt.Sprintf("%s_%s_%s_%s", m.ProjectName, m.Turbine, m.GTags, m.TimeStart.Format("20060102150405"))
+	return m
+}
+
+func (m *MonitoringNotification) RecordID() interface{} {
+	return fmt.Sprintf("%s_%s_%s_%s", m.ProjectName, m.Turbine, m.GTags, m.TimeStart.Format("20060102150405"))
+}
+
+func (m *MonitoringNotification) TableName() string {
+	return "RealMonitoringNotification"
+}
