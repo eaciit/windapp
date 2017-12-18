ccf994634f618c8376f2f665f345f61d72a4e0d0
rinosukmandityo <rino.sukmandityo@gmail.com>
2017-12-18 16:04:28 +0530

    adding min, max, stddev and all temperature tags on databrowser page and make tag selection filterable

--- web/controller/page.go
+++ web/controller/page.go
@@ -84,7 +84,7 @@ func (w *PageController) DataBrowser(r *knot.WebContext) interface{} {
 	r.Config.OutputType = knot.OutputTemplate
 	r.Config.LayoutTemplate = LayoutFile
 	r.Config.ViewName = "page-databrowser.html"
-	return w.GetParams(r, true).Set("ColumnList", GetCustomFieldList()).Set("HDFColList", GetHFDCustomFieldList())
+	return w.GetParams(r, true).Set("ColumnList", GetCustomFieldList()).Set("HFDColList", GetHFDCustomFieldList())
 }
 
 /*func (w *PageController) DataBrowserNew(r *knot.WebContext) interface{} {
