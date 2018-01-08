81888ac04fd7b6487da8e69484d55ae17e4ba277
Erlinda Damayanti <erlindayunilia@gmail.com>
2018-01-08 11:59:20 +0700

    fix date filter

--- web/assets/core/js/page-monitoring/monitoring-alarm.js
+++ web/assets/core/js/page-monitoring/monitoring-alarm.js
@@ -285,10 +285,14 @@ ma.LoadDataAvail = function(projectname, gridType){
                 ma.maxDatetemp = new Date(res.data.Data[1]);
                 app.currentDateData = new Date(res.data.Data[1]);
 
+
                 $('#availabledatestart').html(kendo.toString(moment.utc(ma.minDatetemp).format('DD-MMMM-YYYY')));
                 $('#availabledateend').html(kendo.toString(moment.utc(ma.maxDatetemp).format('DD-MMMM-YYYY')));
 
-                ma.checkCompleteDate()
+                $('#dateStart').data('kendoDatePicker').value( new Date(Date.UTC(moment( ma.maxDatetemp).get('year'),  ma.maxDatetemp.getMonth(),  ma.maxDatetemp.getDate() - 7, 0, 0, 0, 0)));
+                $('#dateEnd').data('kendoDatePicker').value(kendo.toString(moment.utc(res.data.Data[1]).format('DD-MMM-YYYY')));
+
+                ma.checkCompleteDate();
             }
         }         
     });
