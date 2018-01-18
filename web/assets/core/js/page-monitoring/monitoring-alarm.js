ce128d204a2243e53513463e6c563b6c5d18e841
Erlinda Damayanti <erlindayunilia@gmail.com>
2018-01-18 14:03:58 +0700

    add column reduce availability

--- web/assets/core/js/page-monitoring/monitoring-alarm.js
+++ web/assets/core/js/page-monitoring/monitoring-alarm.js
@@ -136,11 +136,22 @@ ma.CreateGridAlarm = function(gridType, param) {
                 style: "text-align:center;"
             },
             width: 90,
-        }, {
+        },{
             field: "alarmdesc",
             title: "Description",
-            width: 330
+            width: 300
         }];
+
+    if(gridType == "alarm"){
+        columns.push({
+            field: "reduceavailability",
+            title: "Reduce Availability",
+            attributes: {
+                style: "text-align:center;"
+            },
+            width: 120,
+        });
+    }
 
     if(gridType == "alarmraw"){
         columns = [{
