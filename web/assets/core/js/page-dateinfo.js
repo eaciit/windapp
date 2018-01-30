d803433245d6b346e4a45592a264ee36631580a9
Erlinda Damayanti <erlindayunilia@gmail.com>
2018-01-30 17:18:24 +0700

    fix date info

--- web/assets/core/js/page-dateinfo.js
+++ web/assets/core/js/page-dateinfo.js
@@ -38,12 +38,7 @@ di.getAvailDate = function (param) {
         }
         
 
-
-       
-
-        var startDate = new Date(Date.UTC(moment(maxDateData).get('year'), maxDateData.getMonth(), maxDateData.getDate() - 7, 0, 0, 0, 0));
-
-        $('#dateStart').data('kendoDatePicker').value(startDate);
+        $('#dateStart').data('kendoDatePicker').value(new Date.UTC(moment(maxDateData).get('year'), maxDateData.getMonth(), maxDateData.getDate() - 7, 0, 0, 0, 0));
         $('#dateEnd').data('kendoDatePicker').value(di.maxDatetemp());
     })
     return reqDate;
