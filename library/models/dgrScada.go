41843403b08c39db1d7906fa6af14ed6c8961d2a
alipsidikp <alip.sidik.p@gmail.com>
2018-02-09 08:43:43 +0700

    implement lul and complete other data

--- library/models/dgrScada.go
+++ library/models/dgrScada.go
@@ -2,51 +2,68 @@ package models
 
 import (
 	. "eaciit/wfdemo-git/library/helper"
+
 	"github.com/eaciit/orm"
 	"gopkg.in/mgo.v2/bson"
 )
 
-type DGRScadaModel struct {
-	orm.ModelBase       `bson:"-",json:"-"`
-	Id                  bson.ObjectId ` bson:"_id" , json:"_id" `
-	DateInfo            DateInfo
-	ProjectName        	string
-	Turbine             string
-	PowerKW             float64
-	Production          float64
-	PCDeviation         float64
-	Revenue         	float64
-	RevenueInLacs       float64
-	OkTime           	float64
-	TrueAvail           float64
-	ScadaAvail          float64
-	MachineAvail        float64
-	GridAvail           float64
-	TotalAvail 			float64
-	PLF        			float64
-	Budget              float64
-	AvgWindSpeed        float64
-	ExpWindSpeed        float64
-	DowntimeHours       float64
-	LostEnergy    		float64
-	RevenueLoss    		float64
-	MachineDownHours    float64
-	GridDownHours       float64
-	otherdowntimehours  float64
-	machinedownloss     float64
-	griddownloss        float64
-	otherdownloss       float64
-	electricallosses    float64
-	productionratio     float64
-	nooffailures  		float64
-	totalminutes		float64
-	DetWindSpeed      []DetWindSpeed
-	totalrows			float64
-}
+// type DGRScadaModel struct {
+// 	orm.ModelBase       `bson:"-",json:"-"`
+// 	Id                  bson.ObjectId ` bson:"_id" , json:"_id" `
+// 	DateInfo            DateInfo
+// 	ProjectName        	string
+// 	Turbine             string
+// 	PowerKW             float64
+// 	Production          float64
+// 	PCDeviation         float64
+// 	Revenue         	float64
+// 	RevenueInLacs       float64
+// 	OkTime           	float64
+// 	TrueAvail           float64
+// 	ScadaAvail          float64
+// 	MachineAvail        float64
+// 	GridAvail           float64
+// 	TotalAvail 			float64
+// 	PLF        			float64
+// 	Budget              float64
+// 	AvgWindSpeed        float64
+// 	ExpWindSpeed        float64
+// 	DowntimeHours       float64
+// 	LostEnergy    		float64
+// 	RevenueLoss    		float64
+// 	MachineDownHours    float64
+// 	GridDownHours       float64
+// 	otherdowntimehours  float64
+// 	machinedownloss     float64
+// 	griddownloss        float64
+// 	otherdownloss       float64
+// 	electricallosses    float64
+// 	productionratio     float64
+// 	nooffailures  		float64
+// 	totalminutes		float64
+// 	DetWindSpeed      []DetWindSpeed
+// 	totalrows			float64
+// }
 
-type DetWindSpeed struct {
-	SumWindSpeed         float64
-	CountWindSpeed		 float64
+type DGRScadaModel struct {
+	orm.ModelBase      `bson:"-",json:"-"`
+	Id                 bson.ObjectId ` bson:"_id" , json:"_id" `
+	DateInfo           DateInfo
+	ProjectName        string
+	Turbine            string
+	TurbineName        string
+	PowerKW            float64
+	Production         float64
+	OkTime             float64
+	TrueAvail          float64
+	ScadaAvail         float64
+	PLF                float64
+	DowntimeHours      float64
+	LostEnergy         float64
+	MachineDownHours   float64
+	GridDownHours      float64
+	Otherdowntimehours float64
+	LoWindTime         float64
 }
 
 func NewDGRScadaModel() *DGRScadaModel {
--- web/controller/analyticDgrReport.go
+++ web/controller/analyticDgrReport.go
@@ -4,10 +4,10 @@ import (
 	. "eaciit/wfdemo-git/library/core"
 	. "eaciit/wfdemo-git/library/models"
 	"eaciit/wfdemo-git/web/helper"
+	"strings"
 
 	"github.com/eaciit/knot/knot.v1"
-	// "github.com/eaciit/toolkit"
-	"strings"
+
 	"time"
 
 	"github.com/eaciit/dbox"
@@ -28,14 +28,14 @@ func (m *AnalyticDgrReportController) GetList(k *knot.WebContext) interface{} {
 	k.Config.OutputType = knot.OutputJson
 
 	p := struct {
-		DateStart   time.Time
-		DateEnd     time.Time
-		Period      string
-		Project string
-		Turbine []interface{}
-		Skip    int
-		Take    int
-		Sort    []Sorting
+		DateStart time.Time
+		DateEnd   time.Time
+		Period    string
+		Project   string
+		Turbine   []interface{}
+		Skip      int
+		Take      int
+		Sort      []Sorting
 	}{}
 
 	e := k.GetPayload(&p)
@@ -47,20 +47,27 @@ func (m *AnalyticDgrReportController) GetList(k *knot.WebContext) interface{} {
 	turbine := p.Turbine
 	project := p.Project
 
-	query := DB().Connection.NewQuery().From(new(DGRScadaModel).TableName()).Skip(p.Skip).Take(p.Take)
+	turbineName, e := helper.GetTurbineNameList(project)
+	if e != nil {
+		return helper.CreateResult(false, nil, e.Error())
+	}
 
+	query := DB().Connection.NewQuery().From("rpt_scadasummarydaily")
 	tStart, tEnd, e := helper.GetStartEndDate(k, p.Period, p.DateStart, p.DateEnd)
-	query = query.Where(dbox.Gte("dateinfo.dateid", tStart))
-	query = query.Where(dbox.Lte("dateinfo.dateid", tEnd))
+
+	_dfilter := make([]*dbox.Filter, 0)
+	_dfilter = append(_dfilter, dbox.Gte("dateinfo.dateid", tStart))
+	_dfilter = append(_dfilter, dbox.Lte("dateinfo.dateid", tEnd))
 
 	if project != "" {
-		query = query.Where(dbox.Eq("projectname", project))
+		_dfilter = append(_dfilter, dbox.Eq("projectname", project))
 	}
 
 	if len(turbine) > 0 {
-		query = query.Where(dbox.In("turbine", turbine...))
+		_dfilter = append(_dfilter, dbox.In("turbine", turbine...))
 	}
 
+	query.Where(dbox.And(_dfilter...))
 
 	if len(p.Sort) > 0 {
 		var arrsort []string
@@ -81,21 +88,25 @@ func (m *AnalyticDgrReportController) GetList(k *knot.WebContext) interface{} {
 	defer csr.Close()
 
 	results := make([]DGRScadaModel, 0)
-	e = csr.Fetch(&results, 0, false)
+	for {
+		result := DGRScadaModel{}
+		e = csr.Fetch(&result, 1, false)
+		if e != nil {
+			break
+		}
+		result.DowntimeHours = (result.GridDownHours + result.MachineDownHours + result.Otherdowntimehours) * 3600
+		result.TurbineName = turbineName[result.Turbine]
 
-	if e != nil {
-		return helper.CreateResult(false, nil, e.Error())
+		results = append(results, result)
 	}
-	results2 := results
 
 	data := struct {
 		Data  []DGRScadaModel
 		Total int
 	}{
-		Data:  results2,
+		Data:  results,
 		Total: csr.Count(),
 	}
 
 	return helper.CreateResult(true, data, "success")
 }
-
