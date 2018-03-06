43904d820b3e637b0a1763e5e2debfae45c0c5db
Erlinda Damayanti <erlindayunilia@gmail.com>
2018-03-06 14:38:46 +0700

    add export pdf

--- web/assets/core/js/page-power-curve-analysis/power-curve.js
+++ web/assets/core/js/page-power-curve-analysis/power-curve.js
@@ -17,6 +17,33 @@ pc.chartType = ko.observable(pc.chartTypeValue.line); // line, scatter
 pc.isSpecific = ko.observable(true);
 pc.isShowDowntime = ko.observable(false);
 
+
+pc.turbine = ko.observableArray([]);
+pc.powerCurveOptions = ko.observable();
+pc.currProject = ko.observable();
+pc.project = ko.observable();
+pc.dateStart = ko.observable();
+pc.dateEnd = ko.observable();
+pc.ss_airdensity = ko.observable(0.0);
+pc.std_airdensity = ko.observable(0.0);
+pc.isDensity = ko.observable(false);
+pc.dataAvail = ko.observable(0.0);
+pc.dataAvailAll = ko.observable(0.0);
+pc.totalAvail = ko.observable(0.0);
+pc.totalAvailAll = ko.observable(0.0);
+pc.viewName = ko.observable();
+pc.totalAvailTurbines = ko.observableArray([]);
+
+pc.LastFilter;
+pc.TableName;
+pc.FieldList;
+pc.ContentFilter;
+pc.LastFilterDetails;
+pc.TableNameDetails;
+pc.FieldListDetails;
+pc.ContentFilterDetails;
+
+
 pc.buildFilterInfo = function() {
     var isValid = $('#pc-is-valid').is(':checked');
     var isDeviation = pc.isDeviation();
@@ -67,6 +94,77 @@ pc.internalRefresh = function(reloadData) {
         }
     }
 }
+
+pc.getPDF = function(selector, detail){
+    app.loading(true);
+    var project = $("#projectList").data("kendoDropDownList").value();
+    var dateStart = $('#dateStart').data('kendoDatePicker').value();
+    var dateEnd = $('#dateEnd').data('kendoDatePicker').value();  
+
+    pc.project(project);
+    pc.dateStart(kendo.toString(dateStart, "dd/MM/yyyy"));
+    pc.dateEnd(kendo.toString(dateEnd, "dd/MM/yyyy"));
+
+    var title = project+"PowerCurve"+kendo.toString(dateStart, "dd/MM/yyyy")+"to"+kendo.toString(dateEnd, "dd/MM/yyyy")+".pdf";
+    if(detail == true){
+        title = project+"_"+pc.chartType()+"DetailPowerCurve"+kendo.toString(dateStart, "dd/MM/yyyy")+"to"+kendo.toString(dateEnd, "dd/MM/yyyy")+".pdf";
+    }
+    kendo.drawing.drawDOM($(selector)).then(function(group){
+        group.options.set("pdf", {
+            paperSize: "auto",
+            margin: {
+                left   : "5mm",
+                top    : "5mm",
+                right  : "5mm",
+                bottom : "5mm"
+            },
+        });
+      kendo.drawing.pdf.saveAs(group, title);
+        setTimeout(function(){
+            app.loading(false);
+        },2000)
+    });
+}
+
+pc.PowerCurveExporttoExcel = function(tipe, isSplittedSheet, isMultipleProject) {
+    app.loading(true);
+    var namaFile = tipe;
+    if (!isSplittedSheet) {
+        namaFile = fa.project + " " + tipe;
+    }
+
+    var param = {
+        Filters: pc.LastFilter,
+        FieldList: pc.FieldList,
+        Tablename: pc.TableName,
+        TypeExcel: namaFile,
+        ContentFilter: pc.ContentFilter,
+        IsSplittedSheet: isSplittedSheet,
+        IsMultipleProject: isMultipleProject,
+    };
+    if (tipe.indexOf("Details") > 0) {
+        var param = {
+            Filters: pc.LastFilterDetails,
+            FieldList: pc.FieldListDetails,
+            Tablename: pc.TableNameDetails,
+            TypeExcel: namaFile,
+            ContentFilter: pc.ContentFilterDetails,
+            IsSplittedSheet: isSplittedSheet,
+            IsMultipleProject: isMultipleProject,
+        };
+    }
+
+    var urlName = viewModel.appName + "analyticpowercurve/genexcelpowercurve";
+    app.ajaxPost(urlName, param, function(res) {
+        if (!app.isFine(res)) {
+            app.loading(false);
+            return;
+        }
+        window.location = viewModel.appName + "/".concat(res.data);
+        app.loading(false);
+    });
+}
+
 pc.initLineChart = function() {
     app.loading(true);
 
@@ -121,6 +219,16 @@ pc.initLineChart = function() {
 
         var dataTurbine = res.data.Data;
         localStorage.setItem("dataTurbine", JSON.stringify(dataTurbine));
+
+        pc.totalAvail(res.data.TotalDataAvail);
+        pc.totalAvailAll(res.data.TotalDataAvail);
+        pc.totalAvailTurbines(res.data.TotalPerTurbine);
+
+        pc.LastFilter = res.data.LastFilter;
+        pc.FieldList = res.data.FieldList;
+        pc.TableName = res.data.TableName;
+        pc.ContentFilter = res.data.ContentFilter;
+
 
         $('#pc-chart').html("");
         $("#pc-chart").kendoChart({
@@ -255,7 +363,7 @@ pc.initLineChart = function() {
                 }
             },
             dataBound : function(){
-                //page.getLegendActive();
+                pc.getLegendActive();
             },
         });
         $("#pc-chart").data("kendoChart").refresh();
@@ -263,6 +371,19 @@ pc.initLineChart = function() {
         app.loading(false);
     });
 }
+
+pc.getLegendActive = function(){
+    var chart = $("#pc-chart").data("kendoChart");
+    var viewModel = kendo.observable({
+      series: chart.options.series,
+      markerColor: function(e) {
+        return e.get("visible") ? e.color : "grey";
+      }
+    });
+
+    kendo.bind($("#legend"), viewModel);
+}
+
 pc.initScatterChart = function() {
     var turbineList = [];
     var kolor = [];
@@ -439,18 +560,31 @@ pc.initScatterChart = function() {
                 }
             },                
             dataBound : function(){
-                // page.getLegendActive();
+                pc.getLegendActive();
             },
         });
 
         app.loading(false);
-        // if (page.showDownTime()) {
-        //     $('#downtime-list').show();
-        // } else {
-        //     $('#downtime-list').hide();
-        // }
-        // page.ShowHideAfterInitChart();
+        if (pc.showDownTime()) {
+            $('#downtime-list').show();
+        } else {
+            $('#downtime-list').hide();
+        }
+        pc.ShowHideAfterInitChart();
     });
+}
+
+pc.ShowHideAfterInitChart = function() {
+    var len = $('input[id*=chk-][type=checkbox]').length;
+    var chart = $("#pc-chart").data("kendoChart");
+    for (var i = 0; i < len; i++) {
+        if (!$('#chk-' + i).is(':checked')) {
+            // console.log(chart.options);
+            chart.options.series[i].visible = false;
+        }
+    }
+    $("#pc-chart").data("kendoChart").redraw();
+    page.getLegendActive();
 }
 pc.initTurbineList = function() {
     var dtTurbines = JSON.parse(localStorage.getItem("dataTurbine"));
@@ -489,6 +623,8 @@ pc.initTurbineList = function() {
             '</div>');
         }
     });
+     pc.dataAvail((totalDataAvailInProject / totalDataShoulBeInProject));
+     pc.dataAvailAll((totalDataAvailInProject / totalDataShoulBeInProject));
 }
 pc.showHideLegend = function(idx) {
     $('#pc-chk-' + idx).trigger('click');
@@ -519,7 +655,7 @@ pc.showHideLegend = function(idx) {
         
         $.each(chks, function(idx, elm){
             var tbName = $(elm).attr('name');
-            var tbAvail = 0;//page.totalAvailTurbines()[tbName];
+            var tbAvail = 0;//pc.totalAvailTurbines()[tbName];
             totalavail += tbAvail.avail;
             totalCount++;
 
@@ -531,18 +667,18 @@ pc.showHideLegend = function(idx) {
 
         if(totalCount > 0) {
             var selectedAvail = totalavail / totalCount;
-            // page.totalAvail(selectedAvail);
+            pc.totalAvail(selectedAvail);
 
             var selectedSampleAvail = (sampleAvail / 100) / sampleCount;
-            // page.dataAvail(selectedSampleAvail);
+            pc.dataAvail(selectedSampleAvail);
         } else {
-            // page.totalAvail(page.totalAvailAll());
-            // page.dataAvail(page.dataAvailAll());
+            pc.totalAvail(pc.totalAvailAll());
+            pc.dataAvail(pc.dataAvailAll());
         }
     }
 
     chart._legendItemClick(idx);
-    // page.getLegendActive();
+    pc.getLegendActive();
 }
 pc.showHideAllLegend = function(e) {
     var chart = $("#pc-chart").data("kendoChart");
@@ -568,9 +704,17 @@ pc.showHideAllLegend = function(e) {
     }
     $('.chk-option').not(e).prop('checked', e.checked);
     chart.redraw();
-    // page.getLegendActive();
+    pc.getLegendActive();
 }
 pc.initElementEvents = function() {
+    var getAd = _.find(fa.rawproject(), function(p) {
+        return p.ProjectId == fa.project
+    });
+    if(getAd!=undefined) {
+        pc.ss_airdensity(getAd.SS_AirDensity);
+        pc.std_airdensity(getAd.STD_AirDensity);
+    }
+
     $('#pc-deviation-opr').on('change', function(){
         if(pc.isDeviation()) {
             pc.internalRefresh();
