76623e9c28e4b01beddcf6b08007b23606235124
rinosukmandityo <rino.sukmandityo@gmail.com>
2018-03-07 10:39:08 +0530

    make circle bigger when hovering on wind dist & nacelle dist page

--- web/assets/core/js/page-analytic-meteorology/nacelle-distribution.js
+++ web/assets/core/js/page-analytic-meteorology/nacelle-distribution.js
@@ -103,6 +103,9 @@ nd.ChartNacelleDistributon =  function () {
                 height: 360
             },
             series: dataSeries,
+            seriesHover: function(e) {
+                $.each(document.getElementsByTagName("circle"), function(idx, value){value.attributes.r.value = "4"});
+            },
             valueAxis: {
                 labels: {
                     format: "{0:p0}",
